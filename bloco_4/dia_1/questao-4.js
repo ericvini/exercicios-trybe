384a327a39dbeb460e0d72ffc36671dc42c74d8b
ericvini <ericcobain3@gmail.com>
2020-08-27 17:50:33 -0300

    Resolvi os exercícios guidados(1 ao 9)

---
+++ bloco_4/dia_1/questao-4.js
@@ -0,0 +1,20 @@
+//positivo e negativo
+
+var valor=0;
+
+
+if(valor>0){
+
+
+console.log("Positive");
+
+}else if(valor==0){
+
+
+    console.log("zero");
+
+}else{
+
+    console.log("negative");
+}
+
---
+++ bloco_4/dia_1/questao-7.js
@@ -0,0 +1,50 @@
+//converte uma nota dada em porcentagem
+
+var nota=10;
+
+
+if(nota<0 || nota>100){
+
+    console.log("erro");
+
+}else if(nota>=90){
+
+
+    console.log("A");
+   
+}else if(nota>=80){
+
+    console.log("B");
+
+}else if(nota>=70){
+
+    console.log("C");
+
+
+}else if(nota>=60){
+
+    console.log("D");
+
+}else if(nota>=50){
+
+    console.log("E");
+
+}else if(nota<50){
+
+    console.log("F");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
