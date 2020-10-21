a0f118cddd14842df01ace740f4eff88baa1e455
ericvini <ericcobain3@gmail.com>
2020-10-21 15:40:58 -0300

    Exercícios bloco 9 dia 1

---
+++ bloco_9/dia_1/exercicio4.js
@@ -0,0 +1,13 @@
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+function sendMarsTemperature(){
+return console.log(`Mars temperature is: ${getMarsTemperature()} degree Celsius`) ;
+}
+
+
+setTimeout(()=>sendMarsTemperature(),messageDelay()) ; // imprime "Mars temperature is: 20 degree Celsius", por exemplo
